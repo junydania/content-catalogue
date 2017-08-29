cc889cd159ed342dd884536a63c7c82ffb56136c
junydania <o.dania@aol.com>
2017-08-29 13:04:23 +0100

    Added new category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table Category' do
+    it { is_expected.to have_db_column :category_name }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :category_name }
+  end
+
 end
