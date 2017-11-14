ee1834a5b7ae7a2e981c61fb62a407e84f325b22
Dania <o.dania@icloud.com>
2017-11-14 21:07:38 +0100

    Edit profile (#44)
    * Create user feature working
    
    * implemented search field for live query
    
    * live search in place before implementing filterrific
    
    * Created named scopes for filterrific
    
    * Integreated filterrific into the controller and view
    
    * adjusted css class in index file
    
    * refactored video search method
    
    * Refactored the js.haml file
    
    * filter and search working
    
    * updated videos view page
    
    * adjusted css settings of homepage
    
    * edit profile branch created
    
    * edit profile tests passed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,17 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    @user = User.find(current_user.id)
+  end
 
+  def update
+    @video = Video.find(params[:id])
+    if @video.update_attributes(video_params)
+      redirect_to  video_path(@video)
+      flash[:notice] = 'Video successfully updated'
+    else
+      render 'edit'
+    end
   end
 
 
