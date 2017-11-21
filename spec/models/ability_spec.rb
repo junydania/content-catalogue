502853f980cd630342b1f2e48e42c6f69a263ec7
junydania <o.dania@aol.com>
2017-11-21 20:48:14 +0100

    rspec ability test passed

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -7,34 +7,47 @@ RSpec.describe Ability, type: :model do
 
   describe 'User Ability' do
     describe "abilities" do
+
+      before(:example) do
+        @video = FactoryGirl.create(:video)
+        @publisher = FactoryGirl.create(:publisher)
+        @category = FactoryGirl.create(:category)
+        @comedian = FactoryGirl.create(:comedian)
+      end
+
       subject(:ability) { Ability.new(user) }
       let (:user) { nil }
 
       context "when user is an administrator" do
         let(:user){ FactoryGirl.create(:administrator) }
-        it { should be_able_to(:update, FactoryGirl.create(:video))}
-        it { should be_able_to(:delete, FactoryGirl.create(:video))}
+        it { should be_able_to(:update, @video)}
+        it { should be_able_to(:delete, @video)}
       end
 
       context "when is a standard user" do
         let(:user){ FactoryGirl.create(:standard) }
-        it { should be_able_to(:update, FactoryGirl.create(:video))}
-        it { should be_able_to(:create, FactoryGirl.create(:video))}
-        it { should be_able_to(:read, FactoryGirl.create(:video))}
-        it { should_not be_able_to(:delete, FactoryGirl.create(:video))}
+        it { should be_able_to(:update, @video)}
+        it { should be_able_to(:create, @video)}
+        it { should be_able_to(:read, @video)}
+        it { should_not be_able_to(:delete, @video)}
+        it { should be_able_to(:read, @publisher)}
       end
 
       context "when is a guest user" do
         let(:user){ FactoryGirl.create(:guest) }
-        it { should_not be_able_to(:update, FactoryGirl.create(:video))}
-        it { should_not be_able_to(:delete, FactoryGirl.create(:video))}
+        it { should_not be_able_to(:update, @video)}
+        it { should_not be_able_to(:delete, @video)}
+        it { should be_able_to(:read, @video)}
+        it { should be_able_to(:read, @publisher)}
+        it { should be_able_to(:read, @category)}
+        it { should be_able_to(:read, @comedian)}
       end
-
-
-
     end
   end
 end
+
+
+
 
 
 
